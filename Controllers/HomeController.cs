60927e107782200eda288cf796931604d3b361ef
Will W <wjwat@onionslice.org>
2022-04-04 18:31:59 -0500

    Update home controller
    Co-Authored-By: Andy Plymate <plymatea@users.noreply.github.com>
    Co-Authored-By: Josh Tillinghast <jwtill@icloud.com>
    Co-Authored-By: Drew Henderson <drewhendersonemail@gmail.com>
    Co-Authored-By: William Jameson <williamjameson90@gmail.com>
    Co-Authored-By: Ryan Bass <probablynotryan@users.noreply.github.com>

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 using SandwichBucket.Models;
@@ -14,17 +15,55 @@ namespace SandwichBucket.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SandwichBucketContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SandwichBucketContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+        // Have a random sandwich, ingredient, and tag shown on the front page
+        // as well as a list of random ingredients and challenge a visitor
+        // to make a sandwich with it
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var sandwichCount = await _db.Sandwiches.CountAsync();
+            var tagCount = await _db.Tags.CountAsync();
+            var ingredientCount = await _db.Ingredients.CountAsync();
+
+            // Count of random items to display on our front page
+            var randomCount = new Random().Next(3, 10);
+            var randomIngredients = new List<Ingredient>();
+
+            for (int x = 0; x < randomCount; x++)
+            {
+                randomIngredients.Add(_db.Ingredients
+                    .Skip(new Random().Next(ingredientCount))
+                    .FirstOrDefault());
+            }
+
+            var model = new HomeViewModel()
+            {
+                Sandwich = await _db.Sandwiches
+                    .Include(sando => sando.Ingredients) //Trying to include ingredients with the sandwich. 
+                    .ThenInclude(join => join.Ingredient)  //Trying to include ingredients with the sandwich. 
+                    .Skip(new Random().Next(sandwichCount))
+                    .FirstOrDefaultAsync(),
+                Ingredient = await _db.Ingredients
+                    .Skip(new Random().Next(ingredientCount))
+                    .FirstOrDefaultAsync(),
+                Tag = await _db.Tags
+                    .Skip(new Random().Next(tagCount))
+                    .FirstOrDefaultAsync(),
+                RandomSandwich = randomIngredients
+            };
+
+            // Pull out random items, pass them into the view
+            return View(model);
         }
 
+        // You have none, welcome to the internet
         public IActionResult Privacy()
         {
             return View();
