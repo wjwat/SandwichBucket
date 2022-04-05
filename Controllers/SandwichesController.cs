84d17844fe444343caec0f779cd3d5f200415a34
Will W <wjwat@onionslice.org>
2022-04-05 16:59:28 -0500

    Add Random endpoint to controller
    Co-Authored-By: Andy Plymate <plymatea@users.noreply.github.com>
    Co-Authored-By: Josh Tillinghast <jwtill@icloud.com>
    Co-Authored-By: Drew Henderson <drewhendersonemail@gmail.com>
    Co-Authored-By: William Jameson <williamjameson90@gmail.com>
    Co-Authored-By: Ryan Bass <probablynotryan@users.noreply.github.com>

--- Controllers/SandwichesController.cs
+++ Controllers/SandwichesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 using SandwichBucket.Models;
@@ -29,12 +30,10 @@ namespace SandwichBucket.Controllers
     // GET Create
     public ActionResult Create()
     {
-      var model = new CreateViewModel() {
-        Ingredients = _db.Ingredients.ToList(),
-        Tags = _db.Tags.ToList()
-      };
+      ViewBag.Ingredients = _db.Ingredients.ToList();
+      ViewBag.Tags = _db.Tags.ToList();
 
-      return View(model);
+      return View();
     }
 
     // POST Create
@@ -74,19 +73,56 @@ namespace SandwichBucket.Controllers
     // GET Edit
     public ActionResult Edit(int id)
     {
-      var model = new SandwichEditViewModel() {
-        Sandwich = _db.Sandwiches.FirstOrDefault(s => s.SandwichId == id),
-        Ingredients = _db.Ingredients.ToList(),
-        Tags = _db.Tags.ToList()
-      };
+      var model = _db.Sandwiches.FirstOrDefault(s => s.SandwichId == id);
+      ViewBag.Ingredients = _db.Ingredients.ToList();
+      ViewBag.Tags = _db.Tags.ToList();
+      ViewBag.SandwichesIngredients = _db.SandwichesIngredients
+          .Where(s => s.SandwichId == id)
+          .Select(i => i.IngredientId)
+          .ToList();
+      ViewBag.SandwichesTags = _db.SandwichesTags
+          .Where(s => s.SandwichId == id)
+          .Select(t => t.TagId)
+          .ToList();
+      // var model = new SandwichEditViewModel() {
+      //   Sandwich = _db.Sandwiches.FirstOrDefault(s => s.SandwichId == id),
+      //   Ingredients = _db.Ingredients.ToList(),
+      //   Tags = _db.Tags.ToList()
+      // };
 
       return View(model);
     }
+
+    // Before Edit
+    // 1 => 0, 2, 4
+    // After Edit
+    // 1 => 1, 2, 4, 6 (Removed 0, and Added 1, 6)
+    // 2nd Edit
+    // 1 => 0, 2, 4 (Added 0, Removed 1, 6)
+
+    // SandwichId=2
+    // &Name=Banh+Mi
+    // &Description=Hell+yes
+    // &Alignment=LawfulEvil
+    // &IngredientId=1
+    // &IngredientId=2
+    // &IngredientId=4
+    // &IngredientId=6
+    // &IngredientId=17
+    // &IngredientId=28
+    // &IngredientId=33
+    // &IngredientId=43
+    // &IngredientId=54
+    // &IngredientId=65
+    // &IngredientId=118
+    // &IngredientId=119&
 
     // POST Edit
     [HttpPost]
     public ActionResult Edit(Sandwich sandwich, int[] TagId, int[] IngredientId)
     {
+      // If this relationship existed before the Edit and doesn't exist at this point
+      // I need to remove it
       _db.SandwichesTags
           .Where(s => s.SandwichId == sandwich.SandwichId && !TagId.Contains(s.TagId))
           .ToList()
@@ -96,6 +132,7 @@ namespace SandwichBucket.Controllers
           .ToList()
           .ForEach(r => _db.SandwichesIngredients.Remove(r));
 
+      // If this relationship doesn't already exist at this point, I need to create it
       foreach (int t in TagId)
       {
         if (_db.SandwichesTags.Any(st => st.SandwichId == sandwich.SandwichId && st.TagId == t))
@@ -138,6 +175,20 @@ namespace SandwichBucket.Controllers
       _db.Sandwiches.Remove(sandwich);
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public async Task<ActionResult<Object>> Random()
+    {
+      var sandwichCount = await _db.Sandwiches.CountAsync();
+      var sandwich = await _db.Sandwiches
+              .Skip(new Random().Next(sandwichCount))
+              .FirstOrDefaultAsync();
+      
+      return Json(new {
+        Name = sandwich.Name,
+        Description = sandwich.Description,
+        Alignment = sandwich.Alignment
+      });
     }
   }
 }
