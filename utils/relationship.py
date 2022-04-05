421b3897d7125139b86b6e95b925062585b5aa6a
Will W <wjwat@onionslice.org>
2022-04-05 09:25:55 -0500

    Small fixes to relationship script
    Co-Authored-By: Andy Plymate <plymatea@users.noreply.github.com>
    Co-Authored-By: Josh Tillinghast <jwtill@icloud.com>
    Co-Authored-By: Drew Henderson <drewhendersonemail@gmail.com>
    Co-Authored-By: William Jameson <williamjameson90@gmail.com>
    Co-Authored-By: Ryan Bass <probablynotryan@users.noreply.github.com>

--- utils/relationship.py
+++ utils/relationship.py
@@ -1,3 +1,9 @@
+"""
+THIS IS QUICK AND DIRTY
+
+Use at your own risk :)
+"""
+
 import sys
 import json
 
@@ -8,37 +14,32 @@ COUNTER = 1
 with open("data/ingredients.txt") as i:
   INGREDIENTS = [x.split(';')[0].strip().upper() for x in i.readlines()]
 
-  # print(INGREDIENTS)
+with open("data/sandwiches.txt") as f:
+  lines = f.readlines()
+  for l in lines:
+    y = l.split(';', 2)
 
-  with open("data/sandwiches.txt") as f:
-    lines = f.readlines()
-    for l in lines:
-      y = l.split(';', 2)
+    sandwich_ingredients = y[2]
 
-      sandwich_ingredients = y[2]
+    if len(y) != 3:
+      print(f'LINE #{lines.index(l)} is missing 3 values: {l.strip()}')
+      sys.exit()
 
-      if len(y) != 3:
-        print(f'LINE #{lines.index(l)} is missing 3 values: {l.strip()}')
-        sys.exit()
+    sandwich_ingredients = sandwich_ingredients.replace('[', '').replace(']', '')
+    sandwich_ingredients = [a.strip().upper() for a in sandwich_ingredients.split(',')]
 
-      sandwich_ingredients = sandwich_ingredients.replace('[', '').replace(']', '')
-      sandwich_ingredients = [a.strip().upper() for a in sandwich_ingredients.split(',')]
+    for s in sandwich_ingredients:
+      try:
+        ROWS.append(
+          {
+            "SandwichIngredientId": COUNTER,
+            "SandwichId": lines.index(l) + 1,
+            "IngredientId": INGREDIENTS.index(s) + 1
+          }
+        )
+      except ValueError:
+        print(f'{lines.index(l)+1} {y[0]}: {s}', file=sys.stderr)
 
-      # print(y[0] + ': ', end='')
-      for s in sandwich_ingredients:
-        try:
-          ROWS.append(
-            {
-              "SandwichIngredientId": COUNTER,
-              "SandwichId": lines.index(l) + 1,
-              "IngredientId": INGREDIENTS.index(s) + 1
-            }
-          )
-        except ValueError:
-          print(f'{lines.index(l)} {y[0]}: {s}')
-
-        COUNTER += 1
-        # print(INGREDIENTS.index(s), end=', ')
-      # print('')
+      COUNTER += 1
 
 print(json.dumps(ROWS, indent=2))
