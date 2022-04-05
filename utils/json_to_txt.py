40628d3457ba6560c151b173bc9076ddf36dff6c
Will W <wjwat@onionslice.org>
2022-04-05 11:17:25 -0500

    WIP styling

--- utils/json_to_txt.py
+++ utils/json_to_txt.py
@@ -1,3 +1,12 @@
+print("""
+This no longer matches what our models look like, unless you're willing to update
+it please do not use.
+""")
+
+# Remove these lines if you want to start using this again
+import sys
+sys.exit()
+
 import json
 
 OUT_DIR = "from_json/"
