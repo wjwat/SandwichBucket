94dece6511033a4827a52a2a0886834fdb3cb632
Will W <wjwat@onionslice.org>
2022-04-05 12:24:08 -0500

    Remove unnecessary comments from Startup
    Co-Authored-By: Andy Plymate <plymatea@users.noreply.github.com>
    Co-Authored-By: Josh Tillinghast <jwtill@icloud.com>
    Co-Authored-By: Drew Henderson <drewhendersonemail@gmail.com>
    Co-Authored-By: William Jameson <williamjameson90@gmail.com>
    Co-Authored-By: Ryan Bass <probablynotryan@users.noreply.github.com>

--- Startup.cs
+++ Startup.cs
@@ -28,27 +28,12 @@ namespace SandwichBucket
       services.AddEntityFrameworkMySql()
           .AddDbContext<SandwichBucketContext>(options =>
               options.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
-      // services.AddIdentity<ApplicationUser, IdentityRole>()
-      //    .AddEntityFrameworkStores<SandwichBucketContext>();
-      //    .AddDefaultTokenProviders();
-      // services.Configure<IdentityOptions>(options =>
-      // {
-      //   options.Password.RequireDigit = false;
-      //   options.Password.RequiredLength = 0;
-      //   options.Password.RequireLowercase = false;
-      //   options.Password.RequireNonAlphanumeric = false;
-      //   options.Password.RequireUppercase = false;
-      //   options.Password.RequiredUniqueChars = 0;
-      // });
-
     }
 
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
-      // app.UseAuthentication();
       app.UseRouting();
-      // app.UseAuthorization();
       app.UseStaticFiles();
 
       app.UseEndpoints(routes =>
