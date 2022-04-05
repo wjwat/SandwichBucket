3cc1b3f4e6ee1aeb1eca5276963b079342a8be4d
Will W <wjwat@onionslice.org>
2022-04-05 09:26:12 -0500

    Update data generation script to include relationships
    Co-Authored-By: Andy Plymate <plymatea@users.noreply.github.com>
    Co-Authored-By: Josh Tillinghast <jwtill@icloud.com>
    Co-Authored-By: Drew Henderson <drewhendersonemail@gmail.com>
    Co-Authored-By: William Jameson <williamjameson90@gmail.com>
    Co-Authored-By: Ryan Bass <probablynotryan@users.noreply.github.com>

--- utils/update-all.sh
+++ utils/update-all.sh
@@ -3,3 +3,4 @@
 python ingredients.py > ../SeedData/Ingredient.json
 python sandwiches.py > ../SeedData/Sandwich.json
 python tags.py > ../SeedData/Tag.json
+python relationship.py > ../SeedData/SandwichIngredient.json
