fbd0caea9712f12ab4cb215a7592a10d45e88f0d
Josh Tillinghast <jwtill@icloud.com>
2022-04-06 11:43:39 -0700

    Add sandwich for scale js form and styling

--- wwwroot/js/scales.js
+++ wwwroot/js/scales.js
@@ -1,25 +1,18 @@
-//import $ from './js/jquery';
-// JavaScript to read input from user
-
-// Define your units of Measurement
-
-
-
-const user_weight = $("#user-weight").val();
-const user_height = $("#user-height").val();
 
 function getSandWeight(sandW, userW) {
-  return userW/sandW;
+  return userW * sandW;
 }
 function getSandHeight(sandH, userH) {
-  return userH/sandW
+  return userH * sandH;
 }
 $(document).ready(function() {
   $("#convert").submit( function() {
     event.preventDefault();
-    const SANDWICH_WEIGHT = 10; // ounces
+    const user_weight = $("#user-weight").val();
+    const user_height = $("#user-height").val(); 
+    const SANDWICH_WEIGHT = .625; //pounds
     const SANDWICH_LENGTH = 10; // inches
-    $("#results").text("You weigh" + getSandWeight(user_weight, SANDWICH_WEIGHT) + "in sandwiches");
+    $("#results").text("You are " + getSandWeight(user_weight, SANDWICH_WEIGHT) + " sandwiches heavy, and " + getSandHeight(user_height, SANDWICH_LENGTH) + " sandwiches tall.");
   });
 });
 
@@ -27,48 +20,3 @@ $(document).ready(function() {
 
 
 
-// Read input, convert to SANDWICH units, display those values back to the user
-// EX: 2lbs = 32oz = 3.2sandwiches
-
-/*
-function findTriType(length_1, length_2, length_3)
-{
-  
-  if ((length_1 === length_2) && (length_2 === length_3))
-  {
-    $("#results").text("it's equilateral!!!");
-  }
-
-  else if (
-     ((length_1 === length_2) && (length_1 != length_3)) 
-  || ((length_2 === length_3) && (length_2 != length_1)) 
-  || ((length_1 === length_3) && (length_1 != length_2))
-  ) 
-  {
-    $("#results").text("it's isosceles!!!");
-  }
-  else if ((length_1 != length_2) && (length_1 != length_3) && (length_2 != length_3))
-  {
-    $("#results").text("it's scalene!!!");
-  }
-  else if ( 
-        ((length_1 + length_2) <= length_3)
-    ||  ((length_2 + length_3) <= length_1)
-    ||  ((length_1 + length_3) <= length_2)
-  )
-  {
-    $("#results").text("NOT a triangle");
-  }
-}
-
-
-$(document).ready(function() {
-  $("#formOne").submit( function() {
-    event.preventDefault();
-    const length1 = $("#side1").val();
-    const length2 = $("#side2").val();
-    const length3 = $("#side3").val();
-    findTriType(length1, length2, length3);
-  });
-});
-*/
