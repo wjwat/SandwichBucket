be3958d615c60e00154f4309467afa8aaa1920e9
Will W <wjwat@onionslice.org>
2022-04-06 10:36:11 -0500

    Update tags controller routes
    Co-Authored-By: Andy Plymate <plymatea@users.noreply.github.com>
    Co-Authored-By: Josh Tillinghast <jwtill@icloud.com>
    Co-Authored-By: Drew Henderson <drewhendersonemail@gmail.com>
    Co-Authored-By: William Jameson <williamjameson90@gmail.com>
    Co-Authored-By: Ryan Bass <probablynotryan@users.noreply.github.com>

--- Controllers/TagsController.cs
+++ Controllers/TagsController.cs
@@ -22,45 +22,22 @@ namespace SandwichBucket.Controllers
     // Index
     public ActionResult Index()
     {
-      List<Ingredient> Ingredients = _db.Ingredients.ToList();
-      return View(Ingredients);
+      List<Tag> Tags = _db.Tags.ToList();
+      return View(Tags);
     }
 
     // GET Create
     public ActionResult Create()
     {
-      var model = new CreateViewModel() {
-        Ingredients = _db.Ingredients.ToList(),
-        Sandwiches = _db.Sandwiches.ToList()
-      };
-
-      return View(model);
+      return View();
     }
 
     // POST Create
     [HttpPost]
-    public ActionResult Create(Tag tag, int[] IngredientId, int[] SandwichId)
+    public ActionResult Create(Tag tag)
     {
       _db.Tags.Add(tag);
       _db.SaveChanges();
-
-      foreach (int i in IngredientId)
-      {
-        _db.IngredientsTags.Add(new IngredientTag() {
-          TagId = tag.TagId,
-          IngredientId = i
-        });
-      }
-
-      foreach (int s in SandwichId)
-      {
-        _db.SandwichesTags.Add(new SandwichTag() {
-          TagId = tag.TagId,
-          SandwichId = s
-        });
-      }
-      _db.SaveChanges();
-
       return RedirectToAction("Index");
     }
 
@@ -74,49 +51,14 @@ namespace SandwichBucket.Controllers
     // GET Edit
     public ActionResult Edit(int id)
     {
-      var model = new TagEditViewModel() {
-        Ingredients = _db.Ingredients.ToList(),
-        Sandwiches = _db.Sandwiches.ToList(),
-        Tag = _db.Tags.FirstOrDefault(t => t.TagId == id)
-      };
+      var model = _db.Tags.FirstOrDefault(t => t.TagId == id);
       return View(model);
     }
 
     // POST Edit
     [HttpPost]
-    public ActionResult Edit(Tag tag, int[] IngredientId, int[] SandwichId)
+    public ActionResult Edit(Tag tag)
     {
-      _db.IngredientsTags
-          .Where(t => t.TagId == tag.TagId && !IngredientId.Contains(t.IngredientId))
-          .ToList()
-          .ForEach(r => _db.IngredientsTags.Remove(r));
-      _db.SandwichesTags
-          .Where(t => t.TagId == tag.TagId && !SandwichId.Contains(t.SandwichId))
-          .ToList()
-          .ForEach(r => _db.SandwichesTags.Remove(r));
-
-      foreach (int i in IngredientId)
-      {
-        if (_db.IngredientsTags.Any(it => it.TagId == tag.TagId && it.IngredientId == i))
-          continue;
-
-        _db.IngredientsTags.Add(new IngredientTag() {
-          IngredientId = i,
-          TagId = tag.TagId
-        });
-      }
-
-      foreach (int s in SandwichId)
-      {
-        if (_db.SandwichesTags.Any(st => st.TagId == tag.TagId && st.SandwichId == s))
-          continue;
-
-        _db.SandwichesTags.Add(new SandwichTag() {
-          TagId = tag.TagId,
-          SandwichId = s
-        });
-      }
-
       _db.Entry(tag).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
