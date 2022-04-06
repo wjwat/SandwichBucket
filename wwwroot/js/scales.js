76d2f3cacee5d7c4cabbbf1223eecae37cfa6191
Josh Tillinghast <jwtill@icloud.com>
2022-04-06 14:23:08 -0700

    WIP

--- wwwroot/js/scales.js
+++ wwwroot/js/scales.js
@@ -1,4 +1,3 @@
-
 function getSandWeight(sandW, userW) {
   return userW * sandW;
 }
@@ -10,8 +9,8 @@ $(document).ready(function() {
     event.preventDefault();
     const user_weight = $("#user-weight").val();
     const user_height = $("#user-height").val(); 
-    const SANDWICH_WEIGHT = .625; //pounds
-    const SANDWICH_LENGTH = 10; // inches
+    const SANDWICH_WEIGHT = .625; 
+    const SANDWICH_LENGTH = 10; 
     $("#results").text("You are " + getSandWeight(user_weight, SANDWICH_WEIGHT) + " sandwiches heavy, and " + getSandHeight(user_height, SANDWICH_LENGTH) + " sandwiches tall.");
   });
 });
