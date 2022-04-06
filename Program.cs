313c42e65b04fe550185a80d02f1af21060ca9e1
Will Watkins <wjwat@onionslice.org>
2022-04-06 16:32:06 -0500

    Remove changes to Program.cs

--- Program.cs
+++ Program.cs
@@ -7,17 +7,11 @@ namespace SandwichBucket
   {
     public static void Main(string[] args)
     {
-//       app.UseStaticFiles(new StaticFileOptions
-// {
-//     ServeUnknownFileTypes = true
-// });
       var host = new WebHostBuilder()
         .UseKestrel()
         .UseContentRoot(Directory.GetCurrentDirectory())
         .UseIISIntegration()
-        
         .UseStartup<Startup>()
-        
         .Build();
 
       host.Run();
