75561db5dc5d48087126a1346e9a503a533c59cf
Ryan Bass <theryanbass@gmail.com>
2022-04-06 12:49:24 -0700

    add 'nother attack-ball'
    Co-Authored-By: William Jameson <williamjameson90@gmail.com>

--- wwwroot/js/battle.js
+++ wwwroot/js/battle.js
@@ -11,9 +11,17 @@ let ww = (parseInt($("#player-level").html()) + 1);
 const c = Math.floor(Math.random() * (parseInt($("#enemy-level").html()) + 1));
 console.log(p + "||" + c);
 if (p > c){
+    $('#attack-ball').addClass("animate");
+    setTimeout (() => {
+      $('#attack-ball').removeClass("animate");
+    }, 500);
   $("#player-level").text(ww);
 } else {
   if (ww > 2){
+    $('#attack-ball').addClass("de-animate");
+    setTimeout (() => {
+      $('#attack-ball').removeClass("de-animate");
+    }, 500);
     $("#player-level").text(1);
   }
 }
