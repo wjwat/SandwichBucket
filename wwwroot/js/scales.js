77928334c8edafec4b02736184e9c4dae17b17df
jwtill <97003141+jwtill@users.noreply.github.com>
2022-04-07 11:12:49 -0700

    Update scales.js

--- wwwroot/js/scales.js
+++ wwwroot/js/scales.js
@@ -1,40 +1,17 @@
-function getWeightInSand(sandwichWeight, userWeight) {
-  return  sandwichWeight * userWeight;
+function getSandHeight(sandW, userW) {
+  return userW * sandW;
 }
 
-function getHeightInSand(sandwichHeight, userHeight) {
-  return  sandwichHeight * userHeight;
+function getSandWeight(sandW, userW) {
+  return userW * sandW;
 }
-
-$(document).ready(function() {
-  $("#convert").submit( function() {
-    
+$(document).ready(function () {
+  $("#convert").submit(function () {
     event.preventDefault();
-    // alert("hay");
-    const weight_unit = $('select[name="weight-unit"]').val();
-    const height_unit = $("#height-unit").val();
-    alert(weight_unit);
-    alert(height_unit); 
-
     const user_weight = $("#user-weight").val();
-    const user_height = $("#user-height").val(); 
-
-
-    if (weight_unit === "lbs") {
-      const SANDWICH_WEIGHT = .625; 
-    } else {
-      const SANDWICH_WEIGHT = .2834952;
-    }
-    alert(SANDWICH_WEIGHT);
-    if (height_unit === "feet") {
-    const SANDWICH_LENGTH = 10; 
-    } else {
-      const SANDWICH_LENGTH = .254;
-    }
-    alert(SANDWICH_LENGTH);
-
-    
-
-    $("#results").text("You are " + getWeightInSand(user_weight, SANDWICH_WEIGHT) + " sandwiches heavy, and " + getHeightInSand(user_height, SANDWICH_LENGTH) + " sandwiches tall.");
+    const user_height = $("#user-height").val();
+    const SANDWICH_WEIGHT = .625;
+    const SANDWICH_LENGTH = 10;
+    $("#results").text("You are " + getSandWeight(user_weight, SANDWICH_WEIGHT) + " sandwiches heavy, and " + getSandHeight(user_height, SANDWICH_LENGTH) + " sandwiches tall.");
   });
 });
